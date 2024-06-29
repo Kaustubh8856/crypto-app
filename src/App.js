f4a1197fe08a6a888510e7a9ed44a15905a09818
Kaustubh <kaustubh8856@gmail.com>
2024-06-29 13:21:26 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,54 @@
-import React from 'react'
-
+import React from "react";
+import { Switch, Route, Link, Routes } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+import {
+  Navbar,
+  Exchanges,
+  Homepage,
+  Cryptocurrencies,
+  CryptoDetails,
+  News,
+} from "./components";
+import "./App.css";
 const App = () => {
   return (
-    <div>
-        <h1>KSW</h1>
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route exact path="/" element={<Homepage />} />
+              <Route exact path="/exchanges" element={<Exchanges />} />
+              <Route
+                exact
+                path="/cryptocurrencies"
+                element={<Cryptocurrencies />}
+              />
+              <Route exact path="/crypto/:coinId" element={<CryptoDetails />} />
+              <Route exact path="/" element={<Homepage />} />
+            </Routes>
+          </div>
+        </Layout>
+        {/* <div className="footer">
+          <Typography.Title
+            level={5}
+            style={{ textAlign: "center", color: "white" }}
+          >
+            Cryptoverse <br />
+            All Rights Reserved
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="exchanges">Exchanges</Link>
+            <Link to="news">News</Link>
+          </Space>
+        </div> */}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { BrowserRouter as Browser } from "react-router-dom";
+
 
 // ReactDOM.render(<App />, document.getElementById("root"));
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App/>
+    <Browser>
+      <App />
+    </Browser>
   </React.StrictMode>
 );
